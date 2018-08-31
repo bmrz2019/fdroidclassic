910287422a00f07efad92de3a9c0ac1265f6b41b
Marcus Hoffmann <bubu@bubu1.eu>
2018-08-31 19:35:53 +0200

    backport installedAppProvider

--- app/src/main/java/org/fdroid/fdroid/data/InstalledAppProvider.java
+++ app/src/main/java/org/fdroid/fdroid/data/InstalledAppProvider.java
@@ -9,15 +9,17 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
-
 import org.fdroid.fdroid.R;
 import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.Schema.InstalledAppTable;
 import org.fdroid.fdroid.data.Schema.InstalledAppTable.Cols;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class InstalledAppProvider extends FDroidProvider {
@@ -77,9 +79,23 @@ public class InstalledAppProvider extends FDroidProvider {
     private static final String PROVIDER_NAME = "InstalledAppProvider";
 
     private static final String PATH_SEARCH = "search";
-    private static final int    CODE_SEARCH = CODE_SINGLE + 1;
+    private static final int CODE_SEARCH = CODE_SINGLE + 1;
 
     private static final UriMatcher MATCHER = new UriMatcher(-1);
+
+    /**
+     * Built-in apps that are signed by the various Android ROM keys.
+     *
+     * @see <a href="https://source.android.com/devices/tech/ota/sign_builds#certificates-keys">Certificates and private keys</a>
+     */
+    private static final String[] SYSTEM_PACKAGES = {
+            "android", // platform key
+            "com.android.email", // test/release key
+            "com.android.contacts", // shared key
+            "com.android.providers.downloads", // media key
+    };
+
+    private static String[] systemSignatures;
 
     static {
         MATCHER.addURI(getAuthority(), null, CODE_LIST);
@@ -100,9 +116,9 @@ public class InstalledAppProvider extends FDroidProvider {
 
     public static Uri getSearchUri(String keywords) {
         return getContentUri().buildUpon()
-            .appendPath(PATH_SEARCH)
-            .appendPath(keywords)
-            .build();
+                .appendPath(PATH_SEARCH)
+                .appendPath(keywords)
+                .build();
     }
 
     public static String getApplicationLabel(Context context, String packageName) {
@@ -112,9 +128,39 @@ public class InstalledAppProvider extends FDroidProvider {
             appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
             return appInfo.loadLabel(pm).toString();
         } catch (PackageManager.NameNotFoundException | Resources.NotFoundException e) {
-            Utils.debugLog(TAG, "Could not get application label", e);
+            Utils.debugLog(TAG, "Could not get application label: " + e.getMessage());
         }
         return packageName; // all else fails, return packageName
+    }
+
+    /**
+     * Add SQL selection statement to exclude {@link InstalledApp}s that were
+     * signed by the platform/shared/media/testkey keys.
+     *
+     * @see <a href="https://source.android.com/devices/tech/ota/sign_builds#certificates-keys">Certificates and private keys</a>
+     */
+    private QuerySelection selectNotSystemSignature(QuerySelection selection) {
+        if (systemSignatures == null) {
+            Log.i(TAG, "selectNotSystemSignature: systemSignature == null, querying for it");
+            HashSet<String> signatures = new HashSet<>();
+            for (String packageName : SYSTEM_PACKAGES) {
+                Cursor cursor = query(InstalledAppProvider.getAppUri(packageName), new String[]{Cols.SIGNATURE},
+                        null, null, null);
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        signatures.add(cursor.getString(cursor.getColumnIndex(Cols.SIGNATURE)));
+                    }
+                    cursor.close();
+                }
+            }
+            systemSignatures = signatures.toArray(new String[signatures.size()]);
+        }
+
+        Log.i(TAG, "excluding InstalledApps signed by system signatures");
+        for (String systemSignature : systemSignatures) {
+            selection = selection.add("NOT " + Cols.SIGNATURE + " IN (?)", new String[]{systemSignature});
+        }
+        return selection;
     }
 
     @Override
@@ -140,13 +186,44 @@ public class InstalledAppProvider extends FDroidProvider {
         return new QuerySelection(Cols.Package.NAME + " = ?", new String[]{packageName});
     }
 
+    private QuerySelection queryAppSubQuery(String packageName) {
+        String pkg = Schema.PackageTable.NAME;
+        String subQuery = "(" +
+                " SELECT " + pkg + "." + Schema.PackageTable.Cols.ROW_ID +
+                " FROM " + pkg +
+                " WHERE " + pkg + "." + Schema.PackageTable.Cols.PACKAGE_NAME + " = ?)";
+        String query = Cols.PACKAGE_ID + " = " + subQuery;
+        return new QuerySelection(query, new String[]{packageName});
+    }
+
     private QuerySelection querySearch(String query) {
         return new QuerySelection(Cols.APPLICATION_LABEL + " LIKE ?",
                 new String[]{"%" + query + "%"});
     }
 
+    private static class QueryBuilder extends org.fdroid.fdroid.data.QueryBuilder {
+        @Override
+        protected String getRequiredTables() {
+            String pkg = Schema.PackageTable.NAME;
+            String installed = InstalledAppTable.NAME;
+            return installed + " JOIN " + pkg +
+                    " ON (" + pkg + "." + Schema.PackageTable.Cols.ROW_ID + " = " +
+                    installed + "." + Cols.PACKAGE_ID + ")";
+        }
+
+        @Override
+        public void addField(String field) {
+            if (TextUtils.equals(field, Cols.Package.NAME)) {
+                appendField(Schema.PackageTable.Cols.PACKAGE_NAME, Schema.PackageTable.NAME, field);
+            } else {
+                appendField(field, InstalledAppTable.NAME);
+            }
+        }
+    }
+
     @Override
-    public Cursor query(Uri uri, String[] projection, String customSelection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(@NonNull Uri uri, String[] projection,
+                        String customSelection, String[] selectionArgs, String sortOrder) {
         if (sortOrder == null) {
             sortOrder = Cols.APPLICATION_LABEL;
         }
@@ -154,6 +231,7 @@ public class InstalledAppProvider extends FDroidProvider {
         QuerySelection selection = new QuerySelection(customSelection, selectionArgs);
         switch (MATCHER.match(uri)) {
             case CODE_LIST:
+                selection = selectNotSystemSignature(selection);
                 break;
 
             case CODE_SINGLE:
@@ -170,33 +248,59 @@ public class InstalledAppProvider extends FDroidProvider {
                 throw new UnsupportedOperationException(message);
         }
 
-        Cursor cursor = db().query(getTableName(), projection, selection.getSelection(), selection.getArgs(), null, null, sortOrder);
+        QueryBuilder query = new QueryBuilder();
+        query.addFields(projection);
+        if (projection.length == 0) {
+            query.addField(Cols._ID);
+        }
+        query.addSelection(selection);
+        query.addOrderBy(sortOrder);
+
+        Cursor cursor = db().rawQuery(query.toString(), selection.getArgs());
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
     }
 
     @Override
-    public int delete(Uri uri, String where, String[] whereArgs) {
+    public int delete(@NonNull Uri uri, String where, String[] whereArgs) {
 
         if (MATCHER.match(uri) != CODE_SINGLE) {
             throw new UnsupportedOperationException("Delete not supported for " + uri + ".");
         }
 
+        String packageName = uri.getLastPathSegment();
         QuerySelection query = new QuerySelection(where, whereArgs);
-        query = query.add(queryApp(uri.getLastPathSegment()));
+        query = query.add(queryAppSubQuery(packageName));
 
-        return db().delete(getTableName(), query.getSelection(), query.getArgs());
+        int count = db().delete(getTableName(), query.getSelection(), query.getArgs());
+
+        AppProvider.Helper.calcSuggestedApk(getContext(), packageName);
+
+        return count;
     }
 
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
 
         if (MATCHER.match(uri) != CODE_LIST) {
             throw new UnsupportedOperationException("Insert not supported for " + uri + ".");
         }
 
+        if (!values.containsKey(Cols.Package.NAME)) {
+            throw new IllegalStateException("Package name not provided to InstalledAppProvider");
+        }
+
+        String packageName = values.getAsString(Cols.Package.NAME);
+        long packageId = PackageProvider.Helper.ensureExists(getContext(), packageName);
+        values.remove(Cols.Package.NAME);
+        values.put(Cols.PACKAGE_ID, packageId);
+
         verifyVersionNameNotNull(values);
+
         db().replaceOrThrow(getTableName(), null, values);
+
+        AppProvider.Helper.calcSuggestedApk(getContext(), packageName);
+
         return getAppUri(values.getAsString(Cols.Package.NAME));
     }
 
@@ -206,8 +310,9 @@ public class InstalledAppProvider extends FDroidProvider {
      * row, if one exists.  This just throws {@link UnsupportedOperationException}
      */
     @Override
-    public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
-        throw new UnsupportedOperationException("\"Update' not supported for installed appp provider. Instead, you should insert, and it will overwrite the relevant rows if one exists.");
+    public int update(@NonNull Uri uri, ContentValues values, String where, String[] whereArgs) {
+        throw new UnsupportedOperationException("\"Update' not supported for installed appp provider."
+                + " Instead, you should insert, and it will overwrite the relevant rows if one exists.");
     }
 
     /**
