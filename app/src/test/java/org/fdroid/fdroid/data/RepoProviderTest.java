ad7509c4f8c16cb06edaeacf4ffba99d27ab8b5b
Hans-Christoph Steiner <hans@eds.org>
2019-05-07 12:38:45 +0200

    Normalize Repo.lastUpdated values to contain the date and time
    This also converts old Repo.lastUpdated values rather than just failing.
    index.xml handling used to store the Repo "Last Updated" date used to store
    the value as just an ISO date (2019-04-29), then the time was added.  So if
    date/time parsing fails, this falls back to trying to parse just the date.
    null is returned when parsing fails, and the Latest Tab shows nothing if
    the Last Updated is null.
    
    Some related tests were also tweaked.
    
    Hopefully:
    closes fdroid/fdroidclient#1757

--- app/src/test/java/org/fdroid/fdroid/data/RepoProviderTest.java
+++ app/src/test/java/org/fdroid/fdroid/data/RepoProviderTest.java
@@ -23,30 +23,82 @@ package org.fdroid.fdroid.data;
 
 import android.app.Application;
 import android.content.ContentValues;
+import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.Nullable;
-
 import org.fdroid.fdroid.BuildConfig;
 import org.fdroid.fdroid.R;
 import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.Schema.RepoTable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-// TODO: Use sdk=24 when Robolectric supports this
-@Config(constants = BuildConfig.class, application = Application.class, sdk = 23)
-@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, application = Application.class)
+@RunWith(RobolectricTestRunner.class)
 public class RepoProviderTest extends FDroidProviderTest {
 
     private static final String[] COLS = RepoTable.Cols.ALL;
+
+    @Test
+    public void countEnabledRepos() {
+
+        // By default, f-droid is enabled.
+        assertEquals(1, RepoProvider.Helper.countEnabledRepos(context));
+
+        Repo gpRepo = RepoProvider.Helper.findByAddress(context, "https://guardianproject.info/fdroid/repo");
+        gpRepo = setEnabled(gpRepo, true);
+        assertEquals(2, RepoProvider.Helper.countEnabledRepos(context));
+
+        Repo fdroidRepo = RepoProvider.Helper.findByAddress(context, "https://f-droid.org/repo");
+        setEnabled(fdroidRepo, false);
+        setEnabled(gpRepo, false);
+
+        assertEquals(0, RepoProvider.Helper.countEnabledRepos(context));
+    }
+
+    private Repo setEnabled(Repo repo, boolean enabled) {
+        ContentValues enable = new ContentValues(1);
+        enable.put(RepoTable.Cols.IN_USE, enabled);
+        RepoProvider.Helper.update(context, repo, enable);
+        return RepoProvider.Helper.findByAddress(context, repo.address);
+    }
+
+    @Test
+    public void lastUpdated() {
+        assertNull(RepoProvider.Helper.lastUpdate(context));
+
+        Repo gpRepo = RepoProvider.Helper.findByAddress(context, "https://guardianproject.info/fdroid/repo");
+
+        // Set date to 2017-04-05 11:56:38
+        setLastUpdate(gpRepo, new Date(1491357408643L));
+
+        // GP is not yet enabled, so it is not counted.
+        assertNull(RepoProvider.Helper.lastUpdate(context));
+
+        // Set date to 2017-04-04 11:56:38
+        Repo fdroidRepo = RepoProvider.Helper.findByAddress(context, "https://f-droid.org/repo");
+        setLastUpdate(fdroidRepo, new Date(1491357408643L - (1000 * 60 * 60 * 24)));
+        assertEquals("2017-04-04", Utils.formatDate(RepoProvider.Helper.lastUpdate(context), null));
+
+        setEnabled(gpRepo, true);
+        assertEquals("2017-04-05", Utils.formatDate(RepoProvider.Helper.lastUpdate(context), null));
+    }
+
+    private Repo setLastUpdate(Repo repo, Date date) {
+        ContentValues values = new ContentValues(1);
+        values.put(RepoTable.Cols.LAST_UPDATED, Utils.formatTime(date, null));
+        RepoProvider.Helper.update(context, repo, values);
+        return RepoProvider.Helper.findByAddress(context, repo.address);
+    }
 
     @Test
     public void findByUrl() {
@@ -89,7 +141,8 @@ public class RepoProviderTest extends FDroidProviderTest {
         for (String url : urls) {
             Repo actualRepo = RepoProvider.Helper.findByUrl(context, Uri.parse(url), COLS);
             assertNotNull("No repo matching URL " + url, actualRepo);
-            assertEquals("Invalid repo for URL [" + url + "]. Expected [" + expectedRepo.address + "] but got [" + actualRepo.address + "]", expectedRepo.id, actualRepo.id);
+            assertEquals("Invalid repo for URL [" + url + "]. Expected [" + expectedRepo.address + "] but got ["
+                    + actualRepo.address + "]", expectedRepo.id, actualRepo.id);
         }
 
     }
@@ -104,16 +157,16 @@ public class RepoProviderTest extends FDroidProviderTest {
         assertEquals(defaultRepos.size(), 4); // based on app/src/main/res/default_repo.xml
 
         String[] reposFromXml = context.getResources().getStringArray(R.array.default_repos);
-        if (reposFromXml.length % DBHelper.REPO_XML_ARG_COUNT != 0) {
+        if (reposFromXml.length % DBHelper.REPO_XML_ITEM_COUNT != 0) {
             throw new IllegalArgumentException(
                     "default_repo.xml array does not have the right number of elements");
         }
-        for (int i = 0; i < reposFromXml.length / DBHelper.REPO_XML_ARG_COUNT; i++) {
-            int offset = i * DBHelper.REPO_XML_ARG_COUNT;
+        for (int i = 0; i < reposFromXml.length / DBHelper.REPO_XML_ITEM_COUNT; i++) {
+            int offset = i * DBHelper.REPO_XML_ITEM_COUNT;
             assertRepo(
                     defaultRepos.get(i),
                     reposFromXml[offset + 1], // address
-                    reposFromXml[offset + 2], // description
+                    reposFromXml[offset + 2].replaceAll("\\s+", " "), // description
                     Utils.calcFingerprint(reposFromXml[offset + 7]), // pubkey
                     reposFromXml[offset]      // name
             );
@@ -126,6 +179,7 @@ public class RepoProviderTest extends FDroidProviderTest {
         assertEquals(4, RepoProvider.Helper.all(context).size());
 
         Repo mock1 = insertRepo(
+                context,
                 "https://mock-repo-1.example.com/fdroid/repo",
                 "Just a made up repo",
                 "ABCDEF1234567890",
@@ -133,6 +187,7 @@ public class RepoProviderTest extends FDroidProviderTest {
         );
 
         Repo mock2 = insertRepo(
+                context,
                 "http://mock-repo-2.example.com/fdroid/repo",
                 "Mock repo without a name",
                 "0123456789ABCDEF"
@@ -168,6 +223,7 @@ public class RepoProviderTest extends FDroidProviderTest {
     @Test
     public void canDeleteRepo() {
         Repo mock1 = insertRepo(
+                context,
                 "https://mock-repo-1.example.com/fdroid/repo",
                 "Just a made up repo",
                 "ABCDEF1234567890",
@@ -175,6 +231,7 @@ public class RepoProviderTest extends FDroidProviderTest {
         );
 
         Repo mock2 = insertRepo(
+                context,
                 "http://mock-repo-2.example.com/fdroid/repo",
                 "Mock repo without a name",
                 "0123456789ABCDEF"
@@ -192,16 +249,23 @@ public class RepoProviderTest extends FDroidProviderTest {
         assertEquals(mock2.id, afterDelete.get(4).id);
     }
 
-    protected Repo insertRepo(String address, String description, String fingerprint) {
-        return insertRepo(address, description, fingerprint, null);
+    public Repo insertRepo(Context context, String address, String description, String fingerprint) {
+        return insertRepo(context, address, description, fingerprint, null);
     }
 
-    protected Repo insertRepo(String address, String description, String fingerprint, @Nullable String name) {
+    public static Repo insertRepo(Context context, String address, String description,
+                                  String fingerprint, @Nullable String name) {
+        return insertRepo(context, address, description, fingerprint, name, false);
+    }
+
+    public static Repo insertRepo(Context context, String address, String description,
+                                  String fingerprint, @Nullable String name, boolean isSwap) {
         ContentValues values = new ContentValues();
         values.put(RepoTable.Cols.ADDRESS, address);
         values.put(RepoTable.Cols.DESCRIPTION, description);
         values.put(RepoTable.Cols.FINGERPRINT, fingerprint);
         values.put(RepoTable.Cols.NAME, name);
+        values.put(RepoTable.Cols.IS_SWAP, isSwap);
 
         RepoProvider.Helper.insert(context, values);
         return RepoProvider.Helper.findByAddress(context, address);
