60606c1b9636ff6a56f87698d44cd5efc048fb26
Marcus Hoffmann <bubu@bubu1.eu>
2020-06-16 13:44:59 +0200

    remove Rx dependency
    This restores the previous hand-coded debounce implementation instead.
    Rx was only used in one place

--- app/src/main/java/org/fdroid/fdroid/data/InstalledAppProviderService.java
+++ app/src/main/java/org/fdroid/fdroid/data/InstalledAppProviderService.java
@@ -14,9 +14,6 @@ import android.util.Log;
 import org.fdroid.fdroid.AppUpdateStatusManager;
 import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.Schema.InstalledAppTable;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
-import rx.subjects.PublishSubject;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -24,6 +21,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -56,51 +55,8 @@ public class InstalledAppProviderService extends JobIntentService {
 
     private static final String EXTRA_PACKAGE_INFO = "org.fdroid.fdroid.data.extra.PACKAGE_INFO";
 
-    /**
-     * This is for notifing the users of this {@link android.content.ContentProvider}
-     * that the contents has changed.  Since {@link Intent}s can come in slow
-     * or fast, and this can trigger a lot of UI updates, the actual
-     * notifications are rate limited to one per second.
-     */
-    private PublishSubject<String> packageChangeNotifier;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        packageChangeNotifier = PublishSubject.create();
-
-        // This "debounced" event will queue up any number of invocations within one second, and
-        // only emit an event to the subscriber after it has not received any new events for one second.
-        // This ensures that we don't constantly ask our lists of apps to update as we iterate over
-        // the list of installed apps and insert them to the database...
-        packageChangeNotifier
-                .subscribeOn(Schedulers.newThread())
-                .debounce(3, TimeUnit.SECONDS)
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String packageName) {
-                        Utils.debugLog(TAG, "Notifying content providers (so they can update the relevant views).");
-                        getContentResolver().notifyChange(AppProvider.getContentUri(), null);
-                        getContentResolver().notifyChange(ApkProvider.getContentUri(), null);
-                    }
-                });
-
-        // ...alternatively, this non-debounced version will instantly emit an event about the
-        // particular package being updated. This is required so that our AppDetails view can update
-        // itself immediately in response to an app being installed/upgraded/removed.
-        // It does this _without_ triggering the main lists to update themselves, because they listen
-        // only for changes to specific URIs in the AppProvider. These are triggered when a more
-        // general notification (e.g. to AppProvider.getContentUri()) is fired, but not when a
-        // sibling such as AppProvider.getHighestPriorityMetadataUri() is fired.
-        packageChangeNotifier.subscribeOn(Schedulers.newThread())
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String packageName) {
-                        getContentResolver()
-                                .notifyChange(AppProvider.getHighestPriorityMetadataUri(packageName), null);
-                    }
-                });
-    }
+    private ScheduledExecutorService worker;
+    private boolean notifyChangeNeedsSending;
 
     /**
      * Inserts an app into {@link InstalledAppProvider} based on a {@code package:} {@link Uri}.
@@ -258,7 +214,10 @@ public class InstalledAppProviderService extends JobIntentService {
                 ausm.updateApk(status.getCanonicalUrl(), AppUpdateStatusManager.Status.InstallError, null);
             }
         }
-        packageChangeNotifier.onNext(packageName);
+        //notify on sub-uri immediately
+        getContentResolver().notifyChange(AppProvider.getHighestPriorityMetadataUri(packageName), null);
+        //notify top level content resolvers ony every 3 seconds
+        notifyChange();
     }
 
     /**
@@ -320,5 +279,31 @@ public class InstalledAppProviderService extends JobIntentService {
         Log.d(TAG, "deleteAppFromDb " + packageName);
         Uri uri = InstalledAppProvider.getAppUri(packageName);
         context.getContentResolver().delete(uri, null, null);
+    }
+
+
+    /**
+     * This notifies the users of this {@link android.content.ContentProvider}
+     * that the contents has changed.  Since {@link Intent}s can come in slow
+     * or fast, and this can trigger a lot of UI updates, the actual
+     * notifications are rate limited to one per second.
+     */
+    private void notifyChange() {
+        notifyChangeNeedsSending = true;
+        Runnable task = () -> {
+            if (notifyChangeNeedsSending) {
+                Utils.debugLog(TAG, "Notifying content providers (so they can update the relevant views).");
+                getContentResolver().notifyChange(AppProvider.getContentUri(), null);
+                getContentResolver().notifyChange(ApkProvider.getContentUri(), null);
+                notifyChangeNeedsSending = false;
+            } else {
+                worker.shutdown();
+                worker = null;
+            }
+        };
+        if (worker == null || worker.isShutdown()) {
+            worker = Executors.newSingleThreadScheduledExecutor();
+            worker.scheduleAtFixedRate(task, 0, 3, TimeUnit.SECONDS);
+        }
     }
 }
